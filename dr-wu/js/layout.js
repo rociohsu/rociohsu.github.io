eba611122abb2ab47e8033bf13b7c462b6de5d3b
Rocio Hsu <shachien@gmail.com>
2026-06-01 17:09:17 +0800

    Update 新增作品
    20260601

---
+++ dr-wu/js/layout.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+var wow = new WOW(
+	{
+	boxClass:     'wow',      // 要套用WOW.js縮需要的動畫class(預設是wow)
+	animateClass: 'animated', // 要"動起來"的動畫(預設是animated, 因此如果你有其他動畫library要使用也可以在這裡調整)
+	offset:       0,          // 距離顯示多遠開始顯示動畫 (預設是0, 因此捲動到顯示時才出現動畫)
+	mobile:       true,       // 手機上是否要套用動畫 (預設是true)
+	live:         true,       // 非同步產生的內容是否也要套用 (預設是true, 非常適合搭配SPA)
+	callback:     function(box) {
+		// 當每個要開始時, 呼叫這裡面的內容, 參數是要開始進行動畫特效的element DOM
+	},
+	scrollContainer: null // 可以設定成只套用在某個container中捲動才呈現, 不設定就是整個視窗
+	}
+);
+wow.init();
+
+$(function() {
+    $('nav.menu .container > a').click(function(){
+        var tabName = $(this).attr('rel');
+        
+        $('html,body').animate({
+            scrollTop: $('#'+tabName).offset().top - 75
+        },300);
+	});
+
+    $('a.top-btn').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        },300);
+	});
+});
+$(window).scroll(function() {
+    if($(window).scrollTop() > 300){
+        $('a.top-btn').stop(false,true).fadeIn();
+    }else{
+        $('a.top-btn').stop(false,true).fadeOut();
+    }
+});
